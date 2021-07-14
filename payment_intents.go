30d6615cd01aa2e950a4400f9e3fb8d58e739905
juddbaguio <juddmisaelbaguio@gmail.com>
2021-07-14 17:34:25 +0800

    feature/payment-intents

---
+++ payment_intents.go
@@ -0,0 +1,13 @@
+package paymongo
+
+func (p *PaymongoInstance) CreatePaymentIntent(data map[string]interface{}) {
+
+}
+
+func (p *PaymongoInstance) RetrievePaymentIntent(data map[string]interface{}) {
+
+}
+
+func (p *PaymongoInstance) AttachPaymentIntent(data interface{}) {
+
+}
